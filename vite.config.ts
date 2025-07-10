580c108624db1dc9edbb264be53ecb5f4ffc07ec
Jody Doolittle <jody@chariotdev.net>
2025-07-10 14:58:48 -0700

    Add chunk banners

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import tailwindcss from "@tailwindcss/vite"
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 import pkg from "./package.json"
+import banner from "vite-plugin-banner"
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		banner(
+			`/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${JSON.stringify(pkg.author)}\n * homepage: ${pkg.homepage}\n */`
+		)
+	],
 	define: {
 		__APP_VERSION__: JSON.stringify(pkg.version),
 		__APP_VERSION_DISPLAY__: JSON.stringify(`v${pkg.version}-alpha`)
